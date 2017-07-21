7cf1d09918c40abb4e614aacf3907999a8c9375c
Viet Le <vietlq85@gmail.com>
2017-07-21 20:55:10 +0100

    /about

--- vslbot.py
+++ vslbot.py
@@ -8,6 +8,23 @@ from bot import util
 
 LOG_CATEGORY = 'VSLBOT.MAIN'
 TELEBOT_WH_PATH = '/%s' % botcfg['TELEBOT_WH_PATH']
+INTRO_MSG = """Hi %s, I'm a bot from VietStartupLondon.
+Here are the commands:
+/about: Discover our projects and community
+/events: Query upcoming and past events
+/member: Register memebership
+/stop: Bot will stop asking you questions
+"""
+ABOUTUS_MSG = """
+Hello %s, we are VietStartupLondon, a vibrant community of young Viet professionals in London.
+We are passionate about entrepreneurship, techonology and working towards connecting Vietnamese start-ups across the globe.
+
+Explore our activities, events and mussings:
+1. Facebook Group: https://www.facebook.com/groups/284739328332602/
+2. Medium: https://medium.com/vietstartup-london
+3. Open Projects: https://trello.com/b/ODvtUhBf/vietstartup
+4. For Developers: https://github.com/VietStartupLondon
+"""
 
 logger = util.get_logger(LOG_CATEGORY)
 sessions = util.get_session_storage()
@@ -33,6 +50,14 @@ def webhook():
         flask.abort(403)
 
 
+def guess_fname(message):
+    '''Guess first name from available data'''
+    if message.chat.first_name:
+        return message.chat.first_name
+    else:
+        return "there"
+
+
 @bot.message_handler(commands=['stop', 'clean'])
 def handle_stop(message):
     '''Stop any pending queries and clear any inline keyboards'''
@@ -42,6 +67,12 @@ def handle_stop(message):
     keyboard = ReplyKeyboardRemove()
     bot.send_message(message.chat.id, 'Talk to you soon!', reply_markup=keyboard)
 
+
+@bot.message_handler(commands=['about', 'intro', 'aboutus'])
+def handle_aboutus(message):
+    '''Introduce VSL to the world'''
+    fname = guess_fname(message)
+    bot.send_message(message.chat.id, ABOUTUS_MSG % fname)
 
 def return_on_stop(f):
     @wraps(f)
@@ -107,17 +138,10 @@ def process_email_step(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_undefined(message):
+    '''Undefined commands or messages go here'''
     chat_id = message.chat.id
+    fname = guess_fname(message)
     logger.info('sessions = %s' % sessions)
     if chat_id in sessions:
         return False
-    if message.chat.first_name:
-        fname = message.chat.first_name
-    else:
-        fname = "there"
-    bot.reply_to(message, """Hi %s, I'm a bot from VietStartupLondon.
-Here are the commands:
-    /events: Query upcoming and past events
-    /member: Register memebership
-    /stop: Bot will stop asking you questions
-""" % fname)
+    bot.reply_to(message, INTRO_MSG % fname)
