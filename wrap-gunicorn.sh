4231c9ef2697f73d909bee2ef1cd4268da1dace5
Viet Le <vietlq85@gmail.com>
2017-07-22 21:12:09 +0100

    workers to 2

--- wrap-gunicorn.sh
+++ wrap-gunicorn.sh
@@ -8,7 +8,7 @@ VSLBOT_ERROR_LOG=${VSLBOT_ERROR_LOG}.${LOG_SUFFIX}
 
 env
 
-gunicorn -b 127.0.0.1:5000 --backlog 100 --workers 4 \
+gunicorn -b 127.0.0.1:5000 --backlog 100 --workers 2 \
     --log-level info --access-logfile $VSLBOT_ACCESS_LOG \
     --error-logfile $VSLBOT_ERROR_LOG \
     wsgi:app
