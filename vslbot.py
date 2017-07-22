dd6660407cf5ccb6bf8db1b5f86c83c2e7ec590b
Viet Le <vietlq85@gmail.com>
2017-07-22 07:56:22 +0100

    cleanup

--- vslbot.py
+++ vslbot.py
@@ -9,9 +9,8 @@ LOG_CATEGORY = 'VSLBOT.MAIN'
 TELEBOT_WH_PATH = '/%s' % botcfg['TELEBOT_WH_PATH']
 INTRO_MSG = """Hi %s, I'm a bot from VietStartupLondon.
 Here are the commands:
-/about: Discover our projects and community
+/about: Discover our community and fun projects
 /events: Query upcoming and past events
-/member: Register memebership
 /stop: Bot will stop asking you questions
 """
 ABOUTUS_MSG = """
@@ -71,59 +70,6 @@ def handle_aboutus(message):
     '''Introduce VSL to the world'''
     fname = guess_fname(message)
     bot.send_message(message.chat.id, ABOUTUS_MSG % fname)
-
-
-@bot.message_handler(commands=['member'])
-def handle_registration(message):
-    chat_id = message.chat.id
-    if chat_id in sessions:
-        return False
-    sessions[chat_id] = util.User(message.chat.first_name, message.chat.last_name)
-    fname = 'there'
-    if message.chat.first_name:
-        fname = message.chat.first_name
-    msg = bot.reply_to(message, """Howdy %s, what's your full name?
-""" % fname)
-    bot.register_next_step_handler(msg, process_name_step)
-
-
-@util.return_on_stop
-def process_name_step(message):
-    chat_id = message.chat.id
-    try:
-        user = sessions[chat_id]
-        user.first_name, user.last_name = message.text.strip().split(' ')
-        # Commit the change
-        sessions[chat_id] = user
-        msg = bot.reply_to(message, 'Your email address, please:')
-        bot.register_next_step_handler(msg, process_email_step)
-    except Exception as e:
-        logger.error('process_name_step: [chat:%d] Could not get first and last names - %s' % (chat_id, e))
-        msg = bot.reply_to(message, 'Please enter first & last names:')
-        bot.register_next_step_handler(msg, process_name_step)
-
-
-@util.return_on_stop
-def process_email_step(message):
-    chat_id = message.chat.id
-    try:
-        if chat_id not in sessions:
-            return False
-        email = message.text.strip().lower()
-        if not util.check_email(email):
-            raise Exception('Invalid email %s' % email)
-        user = sessions[chat_id]
-        user.email = email
-        # Commit the change
-        sessions[chat_id] = user
-        # Do processing
-        bot.send_message(chat_id, 'Nice to meet you, %s. We have recorded your email %s.' % (user.first_name, user.email))
-        # Session is done, clean up
-        del sessions[chat_id]
-    except Exception as e:
-        logger.error('process_email_step: [chat:%d] Could not obtain email - %s' % (chat_id, e))
-        msg = bot.reply_to(message, "Ok, please enter your email:")
-        bot.register_next_step_handler(msg, process_email_step)
 
 
 @bot.message_handler(func=lambda message: True)
