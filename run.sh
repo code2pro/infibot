b9158a015d8cf1da2997dc4f406163a4898cee27
Viet Le <vietlq85@gmail.com>
2017-07-22 16:03:35 +0100

    Added log suffix

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
-gunicorn -b 127.0.0.1:5000 --backlog 100 --workers 4 wsgi:app --log-level info --access-logfile $VSLBOT_ACCESS_LOG --error-logfile $VSLBOT_ERROR_LOG
+LOG_SUFFIX=$(date +%Y%m%d_%H%M%S)
+VSLBOT_ACCESS_LOG=${VSLBOT_ACCESS_LOG}.${LOG_SUFFIX}
+VSLBOT_ERROR_LOG=${VSLBOT_ERROR_LOG}.${LOG_SUFFIX}
+
+gunicorn -b 127.0.0.1:5000 --backlog 100 --workers 4 \
+    --log-level info --access-logfile $VSLBOT_ACCESS_LOG \
+    --error-logfile $VSLBOT_ERROR_LOG \
+    wsgi:app
