f1d0094ec083f92aeac16b1a56b8c4a6e785acf6
Viet Le <vietlq85@gmail.com>
2017-07-23 00:22:59 +0100

    Added membership registration

--- vslbot.py
+++ vslbot.py
@@ -11,6 +11,7 @@ INTRO_MSG = """Hi %s, I'm a bot from VietStartupLondon.
 Here are the commands:
 /about: Discover our community and fun projects
 /events: Query upcoming and past events
+/member: Subscribe to our upcoming events and news
 """
 ABOUTUS_MSG = """
 Hello %s, we are VietStartupLondon, a vibrant community of young Viet professionals in London.
@@ -75,6 +76,74 @@ def handle_events(message):
                 event.name, event.time.strftime("%I:%M%p %a, %b %d %Y"),
                 event.url)
         bot.send_message(message.chat.id, "Our events:\n%s" % output)
+
+
+@bot.message_handler(commands=['member'])
+def handle_registration(message):
+    chat_id = message.chat.id
+    if chat_id in sessions:
+        return False
+    sessions[chat_id] = util.User(message.chat.first_name, message.chat.last_name)
+    fname = 'there'
+    if message.chat.first_name:
+        fname = message.chat.first_name
+    msg = bot.reply_to(message, """Howdy %s, what's your full name?
+""" % fname)
+    bot.register_next_step_handler(msg, process_name_step)
+
+
+@util.return_on_stop
+def process_name_step(message):
+    chat_id = message.chat.id
+    try:
+        user = sessions[chat_id]
+        user.first_name, user.last_name = message.text.strip().split(' ')
+        # Commit the change
+        sessions[chat_id] = user
+        msg = bot.reply_to(message, 'Your email address, please:')
+        bot.register_next_step_handler(msg, process_email_step)
+    except Exception as e:
+        logger.error('process_name_step: [chat:%d] Could not get first and last names - %s' % (chat_id, e))
+        msg = bot.reply_to(message, 'Please enter first & last names:')
+        bot.register_next_step_handler(msg, process_name_step)
+
+
+@util.return_on_stop
+def process_email_step(message):
+    chat_id = message.chat.id
+    bot.send_chat_action(chat_id, 'typing')
+    try:
+        if chat_id not in sessions:
+            return False
+        email = message.text.strip().lower()
+        if not util.check_email(email):
+            raise Exception('Invalid email %s' % email)
+        user = sessions[chat_id]
+        # Check if the email is already registered
+        if util.is_user_subscribed(email):
+            bot.send_message(chat_id,
+                "Thanks %s! Here's news: You are already registered with the email %s" % (
+                user.first_name, user.emai))
+            # Session is done, clean up
+            del sessions[chat_id]
+        # If the email is not registered, proceed
+        user.email = email
+        # Commit the change
+        sessions[chat_id] = user
+        if not util.subscribe_user(user):
+            bot.send_message(chat_id,
+                "Hey %s, there's a problem and we could not register your email %s."
+                    + " Please try again alter. We are really sorry for the inconvenience." % (
+                        user.first_name, user.email))
+        else:
+            bot.send_message(chat_id, 'Nice to meet you, %s. We have recorded your email %s.' % (
+                user.first_name, user.email))
+        # Session is done, clean up
+        del sessions[chat_id]
+    except Exception as e:
+        logger.error('process_email_step: [chat:%d] Could not obtain email - %s' % (chat_id, e))
+        msg = bot.reply_to(message, "Ok, please enter your email:")
+        bot.register_next_step_handler(msg, process_email_step)
 
 
 @bot.message_handler(func=lambda message: True)
